6a146d7471c60629613dd7b44436ab43bd2d0b6e
jens brimberg <jens.brimberg@avanza.se>
2016-02-04 11:49:19 +0100

    Added index page and static content pages

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var request = require('request');
 var cachedRequest = require('cached-request')(request);
 var cacheDirectory = __dirname +"/cache";
 var port = 8080;
 var options = {
-    url: "http://lorempixel.com/640/480/cats/",
+    //url: "http://172.23.0.192/image/jpeg.cgi",
+	url: "http://lorempixel.com/640/480/cats/",
     ttl: 1000 //1 seconds
   };
 cachedRequest.setCacheDirectory(cacheDirectory);
@@ -13,7 +15,11 @@ cachedRequest.setCacheDirectory(cacheDirectory);
 app.get('/kaffecache', function(req, res) {
   cachedRequest(options).pipe(res);
 });
-app.use('/', express.static(__dirname + '/dist'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/script', express.static(__dirname + '/script'));
 
 app.listen(port);
 console.log('Node server is now lostening to port '+port);
